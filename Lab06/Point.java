96d01b2dbfc3e451e4b3987615cc2d73447ca584
Dori Denman <denmand@augsburg.edu>
2023-03-29 10:48:52 -0500

    finish javadoc comments for point  class

--- Lab06/Point.java
+++ Lab06/Point.java
@@ -3,12 +3,16 @@ public class Point {
     /** Making public for each access. */
     public int x = 0;
     public int y = 0;
-    
+    /**
+     * Setting the Y and X values
+     * @param xx
+     * @param yy
+     */
     public Point(int xx, int yy) {
         x = xx;
         y = yy;
     }
-
+    /** Print statment for x and y values  */
     public String toString() {
         return "["+x+","+y+"]";
     }
