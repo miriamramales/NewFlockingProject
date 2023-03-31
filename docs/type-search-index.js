48dfcb5ef4a744e5abd1cfc41c188db76eae9119
Miriam <ramalesm@augsburg.edu>
2023-03-31 15:16:59 -0500

    removing docs

--- docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"BoundingBox"},{"p":"<Unnamed>","l":"Circle"},{"p":"<Unnamed>","l":"CircleModel"},{"p":"<Unnamed>","l":"Controller"},{"p":"<Unnamed>","l":"Point"},{"p":"<Unnamed>","l":"Simulation"},{"p":"<Unnamed>","l":"SimulationGUI"},{"p":"<Unnamed>","l":"Vector"}];updateSearchResults();
